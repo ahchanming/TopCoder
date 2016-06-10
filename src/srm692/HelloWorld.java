9a909f79e4cb626a840ae07898a927488e1c1f80
chanming.cm <chanming.cm@alibaba-inc.com>
2016-06-10 08:40:11 +0800

    HelloWorld

---
+++ src/srm692/HelloWorld.java
@@ -0,0 +1,11 @@
+package srm692;
+
+/**
+ * Created by chanming on 16/6/10.
+ */
+
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.print("Hello World");
+    }
+}
