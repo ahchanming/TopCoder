5099e9b808c76b8731e796bf16a4c5bd08d31613
chanming.cm <chanming.cm@alibaba-inc.com>
2016-06-10 10:17:56 +0800

    srm692

--- src/srm692/HelloWorld.java
+++ src/srm692/HelloWorld.java
@@ -1,5 +1,9 @@
 package srm692;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by chanming on 16/6/10.
  */
@@ -7,5 +11,6 @@ package srm692;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.print("Hello World");
+        List<Integer> list = new ArrayList<Integer>();
     }
 }
