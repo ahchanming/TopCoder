0eb09796ea2ebd73b63c31847232f364f0ca06c4
ahchanming@gmail.com <nemo11092>
2016-08-18 22:59:24 +0800

    一些更新

--- src/srm542/PatrolRoute.java
+++ src/srm542/PatrolRoute.java
@@ -8,39 +8,29 @@ public class PatrolRoute {
     public int countRoutes(int X, int Y, int minT, int maxT){
         int fx[] = new int[2 * X + 1];
         int fy[] = new int[2 * Y + 1];
-        for (int i = 0; i < X; ++i){
-            for (int j = 0; j + 1 <  i; ++j){
-                fx[2 * (i - j)] = (fx[2 * (i - j)] + i - j - 1) % MOD;
+        for (int i = 0; i < X; ++i){ //x3的坐标
+            for (int j = 0; j + 1 <  i; ++j){ //x1的坐标
+                fx[2 * (i - j)] = (fx[2 * (i - j)] + i - j - 1) % MOD; //距离为 2（x3-x1）
             }
-        }
+        }//预处理x轴
         for (int i = 0; i < Y; ++i){
             for (int j = 0; j + 1 < i; ++j){
                 fy[2 * (i - j)] = (fy[2 * (i - j)] + i - j - 1) % MOD;
             }
         }
         int ret = 0;
-        for (int i = 4; i < 2 * X; i++){
+        for (int i = 4; i < 2 * X; i++){//枚举x轴的距离总和
             if (fx[i] <= 0){
                 continue;
-            }
-            int j = Math.max(4, minT - i);
+            } // 如果可能数为0 没有意义进行下去
+            int j = Math.max(4, minT - i); //算一下Y轴最少需要多少
             for (; i + j <= maxT && j <= 2 * Y; ++j){
                 if (fy[j] <= 0){
                     continue;
                 }
-                ret =(int)((6L * fx[i] * fy[j] + ret) % MOD);
+                ret =(int)((6L * fx[i] * fy[j] + ret) % MOD); // 统计结果
             }
         }
-/*        for (int i = 0; i <= 8; ++i){
-            if (fx[i] > 0){
-                System.out.println("fx:[" + i + "]: " + fx[i]);
-            }
-        }
-        for (int i = 0; i <= 8; ++i){
-            if (fy[i] > 0){
-                System.out.println("fy:[" + i +"]: " + fy[i]);
-            }
-        }*/
         return ret;
     }
 
