02577df9723dbb6e33746f698a0063367d2f5bff
ahchanming@gmail.com <nemo11092>
2016-09-02 00:51:53 +0800

    一些更新

--- src/srm331/CarolsSinging.java
+++ src/srm331/CarolsSinging.java
@@ -6,9 +6,45 @@ package srm331;
 public class CarolsSinging {
     int ans;
 
+    private int string2int(String str){
+        int ret = 0;
+        for (int i = 0; i < str.length(); ++i){
+            if (str.charAt(i) == 'Y'){
+                ret += (1 << i);
+            }
+        }
+        return ret;
+    }
+
+    void dfs(int dep, int now, int use, String lyrics[] ,int n, int m){
+        if (use >= ans){
+            return;
+        }
+        if (now == (1 << m) - 1){
+            if (use < ans){
+                ans = use;
+                return;
+            }
+        }
+        if (dep >= n){
+            return;
+        }
+        if ((now | string2int(lyrics[dep])) != now){
+            dfs(dep + 1, (now | string2int(lyrics[dep])), use + 1, lyrics, n, m);
+        }
+        dfs(dep + 1, now, use, lyrics, n, m);
+    }
+
     int choose(String[] lyrics){
         ans = lyrics.length;
-
+        int n = lyrics.length;
+        int m = lyrics[0].length();
+        dfs(0, 0, 0, lyrics, n, m);
         return ans;
+    }
+
+    public static void main(String arg[]){
+        String list[] = {"YNY","NYN","YNY","NYY","NYY","NYN"};
+        System.out.println(new CarolsSinging().choose(list));
     }
 }
--- src/srm547/Pillars.java
+++ src/srm547/Pillars.java
@@ -4,41 +4,36 @@ package srm547;
  * Created by opq.chen on 2016/8/22.
  */
 public class Pillars {
-    public double getExpectedLength(int w, int x, int y){
-        long totalNum = 0;
-        Double totalLenth = 0.0;
-        if (x > y){
-            int t = x;
-            x = y;
-            y = t;
-        }
-        for (int h = y - 1; h > 0; --h){
-            int num = Math.min(y - h, x);
-            totalNum += num;
-//            double tmp = Math.sqrt(1L * w * w + 1L * h * h);
-//            double tmq = totalNum;
-//            totalLenth += tmp * tmq;
-            totalLenth = totalLenth + (Double)Math.sqrt(1L * w * w + 1L *  h * h) * num;
-//            if (h < 92690){
-//                break;
-//            }
-        }
-      //  System.out.println(totalLenth);
-        for (int h = x - 1; h > 0; --h){
-            int num = x - h;
-            totalNum += num;
-            totalLenth += 1.0 * num * Math.sqrt(1L * w * w + 1L * h * h);
-        }
-
-        totalNum += x;
-        totalLenth += x * w;
-        System.out.println(totalLenth + "   " + totalNum);
-        return totalLenth / totalNum;
-    }
-
-    public static void main(String args[]){
-//        System.out.println(Double.MAX_VALUE);
-//        System.out.println(Double.MAX_VALUE / 10);
-        System.out.println(new Pillars().getExpectedLength(1, 99175, 56445));
-    }
+//    public double getExpectedLength(int w, int ya, int yb){
+//        if (ya > yb){
+//            int t = ya;
+//            ya = yb;
+//            yb = t;
+//        }
+//        long totalNum = 0; //线段的数量
+//        Double totalLenth = 0.0; //线段的总长度
+//        for (int h = yb - 1; h > 0; --h){ //枚举"高"， 从yb-1到1
+//            int num = Math.min(yb - h, ya); //线段至多ya条
+//            totalNum += num;
+//            totalLenth = totalLenth + Math.sqrt(1L * w * w + 1L *  h * h) * num;
+//        }
+//
+//      //  System.out.println(totalLenth);
+//        for (int h = x - 1; h > 0; --h){
+//            int num = x - h;
+//            totalNum += num;
+//            totalLenth += 1.0 * num * Math.sqrt(1L * w * w + 1L * h * h);
+//        }
+//
+//        totalNum += x;
+//        totalLenth += x * w;
+//        System.out.println(totalLenth + "   " + totalNum);
+//        return totalLenth / totalNum;
+//    }
+//
+//    public static void main(String args[]){
+////        System.out.println(Double.MAX_VALUE);
+////        System.out.println(Double.MAX_VALUE / 10);
+//        System.out.println(new Pillars().getExpectedLength(1, 99175, 56445));
+//    }
 }
